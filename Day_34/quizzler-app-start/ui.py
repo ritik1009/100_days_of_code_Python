07e79d80dfb2caac0104908b136693e48f742271
Ritik <khandelwalritik560@gmail.com>
2023-03-05 20:23:31 +0530

    Day_34 updated

--- Day_34/quizzler-app-start/ui.py
+++ Day_34/quizzler-app-start/ui.py
@@ -24,31 +24,38 @@ class QuizInterface:
 
         #-------------------- right Button --------------- #
         right_image = PhotoImage(file=r"D:\projects\Udemy\100-days-of-code-Python\Day_34\quizzler-app-start\images\true.png")
-        right_button = Button(image=right_image,command=self.correct_button)
-        right_button.grid(row=3,column=1)
+        self.right_button = Button(image=right_image,command=self.correct_function)
+        self.right_button.grid(row=3,column=1)
 
         #-------------------- wrong Button --------------- #
-        wrong_image = PhotoImage(
-            file=r"D:\projects\Udemy\100-days-of-code-Python\Day_34\quizzler-app-start\images\false.png")
-        wrong_button = Button(image=wrong_image,command=self.wrong_button)
-        wrong_button.grid(row=3,column=2)
+        wrong_image = PhotoImage(file=r"D:\projects\Udemy\100-days-of-code-Python\Day_34\quizzler-app-start\images\false.png")
+        self.wrong_button = Button(image=wrong_image,command=self.wrong_function)
+        self.wrong_button.grid(row=3,column=2)
         self.get_next_question()
     
         self.window.mainloop()
 
     def get_next_question(self):
-        question = self.quiz_brain.next_question()
-        self.canva_question.itemconfigure(self.question,text=question)
+        if self.quiz_brain.still_has_questions():
+            self.canva_question.config(bg="white")
+            question = self.quiz_brain.next_question()
+            self.canva_question.itemconfigure(self.question,text=question)
+            self.update_score()
+        else:
+            self.canva_question.config(bg="orange")
+            self.canva_question.itemconfigure(self.question,text="You've reached the end of the quiz!")
+            self.right_button.config(state="disabled")
+            self.wrong_button.config(state="disabled")
 
     def update_score(self):
         score = self.quiz_brain.score
         self.score_label.config(text=f"Score: {score}")
 
-    def correct_button(self):
+    def correct_function(self):
         self.feedback(self.quiz_brain.check_answer(user_answer="True"))
         
 
-    def wrong_button(self):
+    def wrong_function(self):
         self.feedback(self.quiz_brain.check_answer(user_answer="False"))
         
 
@@ -56,10 +63,9 @@ class QuizInterface:
         print(resp)
         if resp:
             print("inside")
-            self.canva_question.config(bg="green")
+            self.canva_question.config(bg='green')
         else:
             self.canva_question.config(bg="red")
-        self.window.after(2000)
-        self.canva_question.config(bg="white")
-        self.get_next_question()
-        self.update_score()
+        self.window.after(1000, func=self.get_next_question)
+        #self.canva_question.config(bg="white")
+        
